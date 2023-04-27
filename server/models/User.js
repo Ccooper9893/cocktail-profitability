7b1f3fc13de615964304ba9284bb1810c297757d
Cody Cooper <cody_codster@live.com>
2023-04-27 15:22:45 -0500

    Created JWT authorization file and set up express apollo server

---
+++ server/models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true, 
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Please enter a valid email address'],
+    },
+    products: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+    }],
+    recipes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Recipe',
+    }],
+});
+
+// hook checks if the password has been modified before hashing it and updating password on user object
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+const { authMiddleware } = require('./utils/jwt-auth');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+//Allow front-end to pass nested objects/arrays in requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//We will need to add NODE_ENV="production" in .env file when deploying.
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`User GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+
+startApolloServer(typeDefs, resolvers);
